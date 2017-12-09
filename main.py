2b0b26b3df6e7622c41fbec394581a6b93497147
“Suren <suren@jerasoft.net>
2017-12-09 14:59:02 +0200

    parameters change

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
                 input_image: image,
                 correct_label: label,
                 keep_prob: 0.5,
-                learning_rate: 0.00001
+                learning_rate: 0.00005
             }
             _, loss = sess.run([train_op, cross_entropy_loss], feed_dict=feed_dict)
         print ('Epoch: {}; Time: {}; Loss: {}'.format(epoch, time() - _t, loss))
